19df92ca6efe4924b5af6b2a15207fe42159b1c1
TroyDota <troybensonsa@gmail.com>
2020-02-05 21:14:46 +0200

    Update bot.py

--- greenbot/bot.py
+++ greenbot/bot.py
@@ -131,6 +131,7 @@ class Bot:
         return self.discord_bot.get_member(member_id)
 
     def get_member_value(self, key, extra={}):
+        log.info(extra)
         if extra["argument"] != 22: return
         member = self.discord_bot.get_member(extra["argument"][3:][:-1])
         if member:
