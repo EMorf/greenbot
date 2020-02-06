b123e683dc0324c5f6f76c515c99a3c481919686
TroyDota <troybensonsa@gmail.com>
2020-02-06 21:26:43 +0200

    Update __init__.py

--- greenbot/modules/__init__.py
+++ greenbot/modules/__init__.py
@@ -2,9 +2,11 @@ from greenbot.modules.base import BaseModule
 from greenbot.modules.base import ModuleSetting
 from greenbot.modules.base import ModuleType
 
+from greenbot.modules.basic import BasicCommandsModule
 from greenbot.modules.basic.admincommands import AdminCommandsModule
 
 
 available_modules = [
     AdminCommandsModule,
+    BasicCommandsModule,
 ]
