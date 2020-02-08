0de1438a1c00faaa2b30c2b42f0fb9b4737ac3dc
TroyDota <troybensonsa@gmail.com>
2020-02-08 12:16:27 +0200

    Update command.py

--- greenbot/models/command.py
+++ greenbot/models/command.py
@@ -239,9 +239,9 @@ class Command(Base):
         self.level = options.get("level", self.level)
         if "action" in options:
             self.action_json = json.dumps(options["action"])
+            log.info(self.action_json)
             self.action_json["type"] = "privatemessage" if self.action_json["type"] == "Private Message" else "reply"
             self.action = ActionParser.parse(self.action_json, command=self.command)
-        log.info(self.action_json)
         if "extra_args" in options:
             self.extra_args = {"command": self}
             self.extra_args.update(options["extra_args"])
