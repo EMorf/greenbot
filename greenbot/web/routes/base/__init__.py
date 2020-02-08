0f6a5ab5aaffa73ce84c2b0fd11be2e6c9f0077b
TroyDota <troybensonsa@gmail.com>
2020-02-08 08:35:59 +0200

    Update __init__.py

--- greenbot/web/routes/base/__init__.py
+++ greenbot/web/routes/base/__init__.py
@@ -1,11 +1,9 @@
 import greenbot.web.routes.base.commands
 import greenbot.web.routes.base.home
 import greenbot.web.routes.base.login
-import greenbot.web.routes.base.user
 
 
 def init(app):
     greenbot.web.routes.base.commands.init(app)
     greenbot.web.routes.base.home.init(app)
     greenbot.web.routes.base.login.init(app)
-    greenbot.web.routes.base.user.init(app)
