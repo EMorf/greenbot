a7458374cf0f95e7c4f7e1500468862721073896
TroyDota <troybensonsa@gmail.com>
2020-02-15 11:09:56 +0200

    Update functions.py

--- greenbot/functions.py
+++ greenbot/functions.py
@@ -172,7 +172,6 @@ class Functions:
 
     async def func_embed_image(self, args, extra={}):
         log.info(args[0])
-        data = discord.Embed(colour=discord.Colour.dark_gold())
-        data.set_thumbnail(url=args[0])
+        data = discord.Embed()
         data.set_image(url=args[0])
         return None, data
