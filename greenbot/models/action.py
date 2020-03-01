8f820e12fbe143e34eac0e22363432c5ef481f73
TroyDota <troybensonsa@gmail.com>
2020-03-01 19:00:04 +0200

    Update action.py

--- greenbot/models/action.py
+++ greenbot/models/action.py
@@ -112,7 +112,7 @@ class Substitution:
             final_sub = needle
             if filter_name in MappingMethods.subs_methods():
                 log.info(filter_name)
-                embed, resp = MappingMethods.subs_methods()[filter_name](args=array_args, key=key, extra=extra)
+                resp, embed = MappingMethods.subs_methods()[filter_name](args=array_args, key=key, extra=extra)
                 if embed != None:
                     embeds.append(embed)
                 final_sub = resp
