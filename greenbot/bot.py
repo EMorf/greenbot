ae48b338a9b28f24de6d2508b62dca61f0b6acd5
TroyDota <troybensonsa@gmail.com>
2020-03-01 21:06:27 +0200

    Update bot.py

--- greenbot/bot.py
+++ greenbot/bot.py
@@ -114,12 +114,13 @@ class Bot:
     def psudo_level_member(self, member):
         user_level = 100
         for role_id in self.roles.keys():
-            role = self.filters.get_role(role_id, None, {})
+            role = list(self.filters.get_role(role_id, None, {}))[0]
             if not role:
                 continue
 
             if role in member.roles:
                 user_level = max(int(user_level), int(self.roles[role_id]))
+        log.info(user_level)
         return user_level
 
     @property
