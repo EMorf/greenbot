6f87b3acc5e26a0c64f7c5f2088cea550b301e39
TroyDota <troybensonsa@gmail.com>
2020-03-03 21:22:35 +0200

    Update schedule.py

--- greenbot/managers/schedule.py
+++ greenbot/managers/schedule.py
@@ -56,13 +56,13 @@ class ScheduleManager:
 
     @staticmethod
     def execute_now(method, args=[], kwargs={}):
-        job = ScheduledJob("date", method, run_date=utils.now(), args=args, kwargs=kwargs)
+        job = ScheduledJob(run_type="date", method=method, run_date=utils.now(), args=args, kwargs=kwargs)
         ScheduleManager.schedules.append(job)
         return job
 
     @staticmethod
     def execute_delayed(delay, method, args=[], kwargs={}):
-        job = ScheduledJob("date", method, run_date=(utils.now() + datetime.timedelta(seconds=delay)), args=args, kwargs=kwargs)
+        job = ScheduledJob(run_type="date", method=method, run_date=(utils.now() + datetime.timedelta(seconds=delay)), args=args, kwargs=kwargs)
         ScheduleManager.schedules.append(job)
         log.info("job added")
         return job
