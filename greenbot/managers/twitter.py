a0c61b157242ef93776f6ef47f1f247306649fbd
TroyDota <troybensonsa@gmail.com>
2020-03-05 19:48:53 +0200

    Update twitter.py

--- greenbot/managers/twitter.py
+++ greenbot/managers/twitter.py
@@ -126,7 +126,7 @@ class TwitterManager(GenericTwitterManager):
             return
 
         try:
-            ScheduleManager.execute_every(60 * 5, self.check_twitter_connection)
+            ScheduleManager.execute_every(30, self.check_twitter_connection)
         except:
             log.exception("Twitter authentication failed.")
 
