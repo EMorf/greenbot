b556ead1404bc27bf1049482dc6b93b94a2ad5cd
TroyDota <troybensonsa@gmail.com>
2020-03-05 20:36:05 +0200

    Update __init__.py

--- greenbot/modules/__init__.py
+++ greenbot/modules/__init__.py
@@ -6,6 +6,7 @@ from greenbot.modules.basic import BasicCommandsModule
 from greenbot.modules.basic.admincommands import AdminCommandsModule
 from greenbot.modules.advancedadminlog import AdvancedAdminLog
 from greenbot.modules.activitytracker import ActivityTracker
+from greenbot.modules.giveaway import Giveaway
 from greenbot.modules.memes import Memes
 from greenbot.modules.movienight import MovieNight
 from greenbot.modules.remindme import RemindMe
@@ -19,6 +20,7 @@ available_modules = [
     AdvancedAdminLog,
     ActivityTracker,
     BasicCommandsModule,
+    Giveaway,
     Memes,
     MovieNight,
     RemindMe,
