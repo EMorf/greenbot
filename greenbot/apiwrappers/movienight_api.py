0079e91e8ca4cdddca97e6012d61302cad3187e0
TroyDota <troybensonsa@gmail.com>
2020-03-08 17:03:22 +0200

    Update movienight_api.py

--- greenbot/apiwrappers/movienight_api.py
+++ greenbot/apiwrappers/movienight_api.py
@@ -66,7 +66,7 @@ class MovieNightAPI:
     def create_ull_fetch_targets_request(self, target_id=None):
         return {
             "header": self.header,
-            "url": f"{self.host_version}/stream_targets/ull" + f"/{target_id}" if target_id else "",
+            "url": f"{self.host_version}/stream_targets/ull" + (f"/{target_id}" if target_id else ""),
         }
 
     def create_stream_state_request(self, target_id):
